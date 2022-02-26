afa6b7df5ba7ce6b0e478e65c0e1762e45e7bf7b
sanidhyakr18 <sanidhyakr18@gmail.com>
2022-02-26 18:18:10 +0530

    add consent form

--- app/src/main/java/com/sandystudios/copenhagen/MainActivity.kt
+++ app/src/main/java/com/sandystudios/copenhagen/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.sandystudios.copenhagen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
+import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+const val NAME = "name"
+const val AGE = "age"
 
 class MainActivity : AppCompatActivity() {
 
-    private val tvTCE: TextView by lazy {
-        findViewById(R.id.tv_TCE)
+    private val tvDecisive: TextView by lazy {
+        findViewById(R.id.tv_decisive)
     }
 
     private val etName: EditText by lazy {
@@ -21,6 +31,10 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.et_age)
     }
 
+    private val tvConsent: TextView by lazy {
+        findViewById(R.id.tv_consent)
+    }
+
     private val checkBox: CheckBox by lazy {
         findViewById(R.id.checkBox)
     }
@@ -29,23 +43,78 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.btn_start)
     }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spannable = SpannableString("TCE.")
+        val spannable = SpannableString("Decisive.")
         spannable.setSpan(
             ForegroundColorSpan(getColor(R.color.primaryBlue)),
-            3, // start
-            4, // end
+            8, // start
+            9, // end
             Spannable.SPAN_EXCLUSIVE_INCLUSIVE
         )
-        tvTCE.text = spannable
+        tvDecisive.text = spannable
+
+        val str = "CLICK HERE"
+        makeTextLink(tvConsent, str, true, getColor(R.color.primaryBlue))
 
         checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
             btnStart.isEnabled = isChecked
         }
+
+        btnStart.setOnClickListener {
+            completeSetup()
+        }
+    }
+
+    private fun completeSetup() {
+        val name = etName.text.toString()
+        val age = etAge.text.toString()
+        when {
+            name.isEmpty() -> Toast.makeText(this, "Name can not be empty!", Toast.LENGTH_SHORT).show()
+            age.isEmpty() -> Toast.makeText(this, "Age can not be empty!", Toast.LENGTH_SHORT).show()
+            else -> {
+                btnStart.isEnabled = false
+//                val uid = UUID.randomUUID().toString()
+//                val user = User(uid, name, age, 1000, "")
+                val intent = Intent(this, InstructionsActivity::class.java)
+                intent.putExtra(NAME, name)
+                intent.putExtra(AGE, age)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    private fun makeTextLink(
+        textView: TextView,
+        str: String,
+        underlined: Boolean,
+        color: Int?
+    ) {
+        val spannableString = SpannableString(textView.text)
+        val textColor = color ?: textView.currentTextColor
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(textView: View) {
+                startActivity(Intent(this@MainActivity, ConsentActivity::class.java))
+            }
+
+            override fun updateDrawState(drawState: TextPaint) {
+                super.updateDrawState(drawState)
+                drawState.isUnderlineText = underlined
+                drawState.color = textColor
+            }
+        }
+        val index = spannableString.indexOf(str)
+        spannableString.setSpan(
+            clickableSpan,
+            index,
+            index + str.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance()
+        textView.highlightColor = Color.TRANSPARENT
     }
 }
---
+++ app/src/main/java/com/sandystudios/copenhagen/model/User.kt
@@ -0,0 +1,3 @@
+package com.sandystudios.copenhagen.model
+
+data class User(val uid: String, val name: String, val age: Int, val score: Int, val gambleChoice: String)
